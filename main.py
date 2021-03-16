8c130f9b4aa389fdc0e02f16827df048bf828039
Yusuf Nevzat Sengun <yusuf.nevzat.sengun@gmail.com>
2021-03-16 19:43:00 +0300

    part-b

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import socket
+import base64
+import re
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8000
+
+ACC_NAME = 'bilkentstu'
+ACC_PASS = 'cs421s2021'
+
+
+class Response:
+    def __init__(self, headers, body, statusCode):
+        self.headers = headers
+        self.body = body
+        self.statusCode = int(statusCode)
 
 
 def listToString(lines):
@@ -11,6 +23,11 @@ def listToString(lines):
         ret += item + '\n'
 
     return ret
+
+
+def writeToFile(fileName, dataToWrite):
+    with open(fileName, 'w') as f:
+        f.write(dataToWrite)
 
 
 def parseResponse(response):
@@ -23,10 +40,7 @@ def parseResponse(response):
     body = lines[split + 1:]
 
     keywords = headers[0].split(' ')
-    if keywords[1] == '200':
-        return headers, body
-    else:
-        return [], []
+    return Response(headers, body, keywords[1])
 
 
 def sendAndReceiveResponse(socket, request):
@@ -40,63 +54,65 @@ def executePartA(socket):
     req = 'GET / HTTP/1.1\r\n'
     req += 'Host: localhost:8000\r\n\r\n'
 
-    headers, body = sendAndReceiveResponse(socket, req)
+    response = sendAndReceiveResponse(socket, req)
 
-    data = listToString(body)
-    with open('index2.html', 'w') as f:
-        f.write(data)
+    data = listToString(response.body)
+    writeToFile('index2.html', data)
 
-    return
+    link = re.findall('\".*html\"', data)[0][1:-1]
+    return '/' + link
 
 
-def executePartB():
-    return
+def executePartB(socket, link, authorization=False):
+    req = 'GET {} HTTP/1.1\r\n'.format(link)
+    req += 'Host: localhost:8000\r\n'
+    if authorization:
+        encoded = base64.b64encode('{}:{}'.format(
+            ACC_NAME, ACC_PASS).encode('ascii')).decode('ascii')
+        req += 'Authorization: Basic {}\r\n\r\n'.format(encoded)
+
+    response = sendAndReceiveResponse(socket, req)
+
+    if response.statusCode == 401:
+        print('Authentication failed')
+    elif response.statusCode == 200:
+        writeToFile('protected2.html', listToString(response.body))
+
+    return None
 
 
 def executePArtC():
     return
 
 
+def exitProgram(socket):
+    req = 'EXIT HTTP/1.1\r\n'
+    req += 'Host: localhost:8000 \r\n\r\n'
+    socket.sendall(bytes(req, 'utf-8'))
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((SERVER_HOST, SERVER_PORT))
 
-    executePartA(s)
-    #req = 'GET / HTTP/1.1\r\n'
-    #req += 'Host: localhost:8000\r\n\r\n'
+    hiddenLink = executePartA(s)
+    print(hiddenLink)
 
-    #s.sendall(bytes(req, 'utf-8'))
-    #data = s.recv(1024)
+    executePartB(s, hiddenLink, False)
+    executePartB(s, hiddenLink, True)
 
-    #data = data.decode('utf-8')
+    # req = 'GET /protected.html HTTP/1.1\r\n'
+    # req += 'Host: localhost:8000 \r\n\r\n'
 
+    # s.sendall(bytes(req, 'utf-8'))
+    # data = s.recv(1024)
+
+    # data = data.decode('utf-8')
     # print(data)
 
-    #lines = data.split('\n')
-    #lines = lines[2:]
+    # lines = data.split('\n')
+    # lines = lines[2:]
 
-    #data = listToString(lines)
+    # data = listToString(lines)
+    # print(data)
 
-    # print(lines)
-
-    # with open('index2.html', 'w') as f:
-    #  f.write(data)
-
-    req = 'GET /protected.html HTTP/1.1\r\n'
-    req += 'Host: localhost:8000 \r\n\r\n'
-
-    s.sendall(bytes(req, 'utf-8'))
-    data = s.recv(1024)
-
-    data = data.decode('utf-8')
-    print(data)
-
-    lines = data.split('\n')
-    lines = lines[2:]
-
-    data = listToString(lines)
-    print(data)
-
-    req = 'EXIT HTTP/1.1\r\n'
-    req += 'Host: localhost:8000 \r\n\r\n'
-
-    s.sendall(bytes(req, 'utf-8'))
+    exitProgram(s)
